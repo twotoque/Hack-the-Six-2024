bc324e229b0cb261f21e1a34917165403890c850
Derek Song <dereksong28@gmail.com>
2024-08-03 17:30:32 -0400

    movie.js page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -29,7 +29,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
-            <Route path="about" element={<Movie />} />
+            <Route path="/pages/Movie" element={<Movie />} />
           </Route>
         </Routes>
       </Router>
