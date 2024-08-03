a2d998c7dc7158f7d8efd2a9f0a7d4fdfdf468f5
WillGotlib <37600983+WillGotlib@users.noreply.github.com>
2024-08-03 17:42:49 -0400

    cors, changed port...

--- backend/index.js
+++ backend/index.js
@@ -5,26 +5,31 @@ const app = express()
 
 const authConfig = {
   auth0Logout: true,
-  secret: '6ijXxBYDhbK6m0pljN8GCbxTx2BQEtqst6XL84zkvkbnJ7h2q5Uf1RwlJJ_6AZxi',
+  secret: "6ijXxBYDhbK6m0pljN8GCbxTx2BQEtqst6XL84zkvkbnJ7h2q5Uf1RwlJJ_6AZxi",
   issuerBaseURL: "https://dev-0oanh27cotux4lfn.us.auth0.com",
-  clientID: 'BNuiJNoPzOg6Z0asnKphqPHQonRZ9yPp',
-  baseURL: 'http://localhost:3000'
+  clientID: "BNuiJNoPzOg6Z0asnKphqPHQonRZ9yPp",
+  baseURL: "http://localhost:3008",
 }
 
 app.use(auth(authConfig))
 
-app.get('/', (req, res) => {
-  res.send(req.oidc.isAuthenticated() ? 'Logged in' : 'Logged out');
-});
+app.get("/", (req, res) => {
+  res.send(req.oidc.isAuthenticated() ? "Logged in" : "Logged out")
+})
+
+var cors = require("cors")
+app.use(cors())
 
 // When we turn into production mode we want to restrict a few endpoints...
 const PRODUCTION = false
 
 // Routes
 const userRoutes = require("./user/index.js")
+const movieRoutes = require("./movie/index.js")
 const screeningRoutes = require("./screening/index.js")
 const venueRoutes = require("./venue/index.js")
 app.use("/user", userRoutes)
+app.use("/movie", movieRoutes)
 app.use("/screening", screeningRoutes)
 app.use("/venue", venueRoutes)
 
@@ -37,11 +42,12 @@ app.get("/", (req, res) => {
 
 // Body-parser middleware
 var bodyParser = require("body-parser")
-const { toBeRequired } = require("@testing-library/jest-dom/matchers.js")
+const { toBeRequired } = require("@testing-library/jest-dom/matchers")
+
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 3008
 
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}/`)
