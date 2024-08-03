f8ec75553856ef2475814c07e4337e5f52631f3f
WillGotlib <37600983+WillGotlib@users.noreply.github.com>
2024-08-03 04:21:13 -0400

    setup for user endpoints for tomorrow

--- backend/user/index.js
+++ backend/user/index.js
@@ -2,25 +2,30 @@ const express = require("express")
 const mongoose = require("mongoose")
 const { auth } = require("express-oauth2-jwt-bearer")
 const User = require("../models/user")
-const { default: connect } = require("../db")
+const connect = require("../db")
 
 const router = express.Router()
 
 const authConfig = {
-  audience: "https://tifftok.com/login",
+  // audience: "http://localhost:3000", //
+  audience: "https://tifftok.com/login", // TODO: Probably need to change this?
   issuerBaseURL: "dev-nr6w2ef4fy5je1rv.ca.auth0.com/",
 }
 
-router.use(auth(authConfig))
+// router.use(auth(authConfig))
 
 router.get("/login", (req, res) => {
   connect()
-  const user = req.user
-  user.findOne({ auth0Id: user.sub }, (err, userProfile) => {
+  const currentUser = req.user
+  User.findOne({ auth0Id: currentUser.sub }, (err, userProfile) => {
     if (err) return res.status(500).send(err)
     if (!userProfile) {
       // create new user profile in MongoDB
-      const newUser = new User({ auth0Id: user.sub, email: user.email, password: user.password })
+      const newUser = new User({
+        auth0Id: currentUser.sub,
+        email: currentUser.email,
+        password: currentUser.password,
+      })
       newUser.save((err, savedUser) => {
         if (err) return res.status(500).send(err)
         res.json(savedUser)
@@ -33,11 +38,45 @@ router.get("/login", (req, res) => {
 
 router.get("/schedule", (req, res) => {
   const user = req.user
-  user.findOne({ auth0Id: user.sub }, (err, userProfile) => {
+  if (!user) {
+    return res.redirect("/login")
+  }
+  User.findOne({ auth0id: user.sub }, (err, userProfile) => {
     if (err) return res.status(500).send(err)
-    if (!userProfile) return res.redirect("/login")
     // Assuming at this point that we're logged in
     return res.json(userProfile.schedule)
+  })
+})
+
+var bodyParser = require("body-parser")
+var jsonParser = bodyParser.json()
+
+// TODO: Make this add a screening to a user's schedule...also make a complementary one to delete
+router.post("/schedule", jsonParser, (req, res) => {})
+router.delete("/schedule", jsonParser, (req, res) => {})
+
+// TODO: Get all this user's friends (redir to login if needed)
+router.get("/friends", (req, res) => {})
+
+// TODO: Should we even be able to look at any other user's friends?
+
+// TODO: Add a friend, remove a friend
+router.post("/friends", (req, res) => {})
+router.delete("/friends", (req, res) => {})
+
+// Return the schedule of an arbitrary user.
+router.get("/schedule/:id", (req, res) => {
+  let auth0id = req.params.id
+  User.findOne({ auth0id: auth0id }, (err, userProfile) => {
+    if (err || !userProfile) return res.status(500).send(err)
+    return res.json(userProfile.schedule)
+  })
+})
+
+router.get("/all", (req, res) => {
+  User.find({}, (err, users) => {
+    if (err) return res.status(500).send(err)
+    return res.json(users)
   })
 })
 
