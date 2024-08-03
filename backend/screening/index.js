8539dadbe93bbe24d462a9fb77470bb998fbf7a3
WillGotlib <37600983+WillGotlib@users.noreply.github.com>
2024-08-03 04:24:42 -0400

    some screening endpoints for tomorrow

--- backend/screening/index.js
+++ backend/screening/index.js
@@ -2,9 +2,17 @@ const express = require("express")
 
 const router = express.Router()
 
+// TODO: Get all screenings
 router.get("/", (req, res) => {
   console.log("Reached screening/")
   return res.json({ screening: "fgrkjhkr" })
 })
+
+// TODO: Add or modify a screening. (Not essential bc this data shouldn't need to change...but would be nice)
+router.post("/", (req, res) => {})
+
+// TODO: Either book a spot or give one up if they already had one booked...reduce the number of seats left in DB accordingly.
+// This is probably unnecessary and doesn't make sense bc you're just expressing interest, not actually buying a ticket by clicking on an event.
+router.post("/bookSeat", (req, res) => {})
 
 module.exports = router
