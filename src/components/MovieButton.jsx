fb5b9b32818c2d08808ebcca466fb9b1115bcdf5
Derek Song <dereksong28@gmail.com>
2024-08-02 23:11:11 -0400

    more front end!!!

--- src/components/MovieButton.jsx
+++ src/components/MovieButton.jsx
@@ -13,8 +13,8 @@ function MovieButton({movie}) {
     <>
     <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center">
         <div className="">
-            <img src= {movie.image}></img>
-            <div class="text-xl font-medium text-right">{movie.title}</div>
+            <img class = "max-h-45" src= {movie.image}></img>
+            <div class="text-xl font-medium text-right ">{movie.title}</div>
         </div>
     </div>
     </>
