afa53276f4a346bb62297b51fde723e4bec3f56a
neiloy <neiloychaudhuri@gmail.com>
2024-08-03 04:10:53 -0400

    server flask

--- public/Auth0/server.py
+++ public/Auth0/server.py
@@ -55,7 +55,7 @@ def logout():
 
 @app.route("/")
 def home():
-    return render_template("index.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
+    return render_template("home.html", session=session.get('user'), pretty=json.dumps(session.get('user'), indent=4))
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=env.get("PORT", 3000))
