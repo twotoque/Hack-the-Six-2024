e211b675dcacdc4139107b23cfa39c071f7bd121
Derek Song <dereksong28@gmail.com>
2024-08-02 22:43:29 -0400

    adding movie image but the way it is set up might not be the best

--- src/App.js
+++ src/App.js
@@ -2,9 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 import MovieButton from './components/MovieButton';
+import badlandsImage from './badlands.jpg';
 
 function App() {
-  const movieData = { title: "test movie", id: 12 };
+  const movieData = { title: "test movie", image: badlandsImage, id: 12 };
   return (
     <div className="App">
       <MovieButton movie={movieData} />
--- src/components/MovieButton.jsx
+++ src/components/MovieButton.jsx
@@ -1,11 +1,21 @@
 import React from "react";
 import { useState } from "react";
 
+const inputStyle = {
+  backgroundColor: '#EDECEC',
+};
+
 function MovieButton({movie}) {
+    // Movie object: 
+    // movie.title 
+    // movie.image 
   return (
     <>
     <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-lg flex items-center">
-        <div class="text-xl font-medium text-right">{movie.title}</div>
+        <div className="">
+            <img src= {movie.image}></img>
+            <div class="text-xl font-medium text-right">{movie.title}</div>
+        </div>
     </div>
     </>
   );
