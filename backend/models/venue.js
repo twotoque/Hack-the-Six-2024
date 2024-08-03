cbf3aca721ba748cd920728c3ac6bb2d570f3b97
WillGotlib <37600983+WillGotlib@users.noreply.github.com>
2024-08-03 02:38:24 -0400

    the truth is vanishing

--- backend/models/venue.js
+++ backend/models/venue.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema
 
 const venueSchema = new mongoose.Schema({
   name: String, // i.e. "TIFF Lightbox"
-  slug: { type: String, unique: True }, // i.e. "tiff-lightbox"
+  slug: { type: String, unique: true }, // i.e. "tiff-lightbox"
   address: String, // i.e. "350 King St W, Toronto"
   description: { type: String, default: "A Movie Theatre in Downtown Toronto." }, // i.e. "A good movie theatre downtown specializing in X and Y."
 })
