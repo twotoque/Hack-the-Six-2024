0ecf9288ad4257beace6395ce37ba5418a538839
Derek Song <dereksong28@gmail.com>
2024-08-03 00:54:35 -0400

    fixed imports

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
-import MovieButton from './components/MovieButton';
-import ProfileButton from './components/ProfileButton';
+import MovieButton from './components/MovieButton.jsx';
+import ProfileButton from './components/ProfileButton.jsx';
 import badlandsImage from './badlands.jpg';
 import profileImage from './profileImage.png';
 
