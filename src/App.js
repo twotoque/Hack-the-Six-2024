75fa18139801fa5d38e97f7d8e1f7360a0aac75d
Derek Song <dereksong28@gmail.com>
2024-08-02 23:54:58 -0400

    started work on profile button

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 import {useState} from 'react';
 import MovieButton from './components/MovieButton';
+import ProfileButton from './components/ProfileButton';
 import badlandsImage from './badlands.jpg';
+import profileImage from './profileImage.png';
 
 function App() {
 
@@ -13,9 +15,16 @@ function App() {
     { title: "test movie 2", image: badlandsImage, id: 14 },
     { title: "test movie 2", image: badlandsImage, id: 15 }
   ]);
+
+  const profile = { name: "John Doe", image: profileImage, id: 1234 };
+  
   return (
     <div className="App">
-        <h1 className="text-7xl font-bold text-left">Tiff-Tok</h1>
+        <div class="flex flex-row items-start">
+          <h1 className="text-7xl font-bold text-left">Tiff-Tok</h1>
+        <ProfileButton key={profile.id} profile={profile} />
+        </div>
+        <h3 className="text-3xl text-left pt-4">An easy way to RSVP and view movie times</h3>
         
         <div class="flex space-x-4">
           {movieData.map(movie => (
