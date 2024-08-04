8cb4d223b39902e738fd4173cae24b8c0160d483
WillGotlib <37600983+WillGotlib@users.noreply.github.com>
2024-08-03 20:27:16 -0400

    more tailwind colours

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,16 +1,27 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {
       maxHeight: {
-        '16': '16rem', 
-      },},
-      colors: {
-        'background-orange': '#fdcfbc',
-        'white' : '#FFFFFF',
+        16: "16rem",
       },
+    },
+    colors: {
+      "background-orange": "#fdcfbc",
+      white: "#FFFFFF",
+      gray: {
+        100: "#f5f5f5",
+        200: "#eeeeee",
+        300: "#e0e0e0",
+        400: "#bdbdbd",
+        500: "#9e9e9e",
+        600: "#757575",
+        700: "#616161",
+        800: "#424242",
+        900: "#212121",
+      },
+    },
   },
   plugins: [],
 }
-
