5e7438543cc4b9d0a6a5a35aa9817db75c7dce75
WillGotlib <37600983+WillGotlib@users.noreply.github.com>
2024-08-03 23:49:05 -0400

    more stuff in bad default profile page

--- frontend/src/pages/Profile/Profile.js
+++ frontend/src/pages/Profile/Profile.js
@@ -8,12 +8,27 @@ const Profile = () => {
     return <div>Loading ...</div>
   }
 
+  const addFriend = () => {}
+
+  user.app_metadata = { friends: ["1", "2"] }
+
   return (
     isAuthenticated && (
       <div>
         <img src={user.picture} alt={user.name} />
         <h2>{user.name}</h2>
         <p>{user.email}</p>
+        <p>App Metadata: {JSON.stringify(user.app_metadata)}</p>
+        <button
+          onClick={() => {
+            console.log(user.app_metadata)
+            user.app_metadata = { friends: [...user.app_metadata.friends, "extra"] }
+          }}
+        >
+          Add Friend
+        </button>
+        {user.picture}
+        <p>{user.nickname}</p>
       </div>
     )
   )
